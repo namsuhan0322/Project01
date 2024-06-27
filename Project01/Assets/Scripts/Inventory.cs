e309f7f05a33b230c8c52b00f4e8ad947ce5353d
namsuhan0322 <rnatpakfl@gmail.com>
2024-06-27 21:51:36 +0900

    [FIx] 주석삭제, 변수이름 수정

--- Project01/Assets/Scripts/Inventory.cs
+++ Project01/Assets/Scripts/Inventory.cs
@@ -7,13 +7,13 @@ public class Inventory : MonoBehaviour
     public static bool inventoryActivated = false;  // 인벤토리 활성화 여부. true가 되면 카메라 움직임과 다른 입력을 막을 것이다.
 
     [SerializeField]
-    private GameObject go_SlotsParent;  // Slot들의 부모인 Grid Setting 
+    private GameObject inventory_Slots;
 
     private Slot[] slots;  // 슬롯들 배열
 
     void Start()
     {
-        slots = go_SlotsParent.GetComponentsInChildren<Slot>();
+        slots = inventory_Slots.GetComponentsInChildren<Slot>();
     }
 
     public void AddItemToInventory(Item item)
